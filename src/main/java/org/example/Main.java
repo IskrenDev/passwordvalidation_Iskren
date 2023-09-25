c282b24a4dfcf9e6c69191c6ad3ec02e951f88cb
iskrenr <iskrenr005@gmail.com>
2023-09-25 14:55:47 +0200

    Neue Methoden

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,59 @@
 package org.example;
 
+
+import static java.lang.Character.isUpperCase;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(checkForNumbersInPassword("abc0"));
     }
 
+
+
+    //checkPasswordLength
     public static int checkPasswordlength(String password) {
         return 6;
     }
+
+    //checkForNumbersInPassword
+    public static boolean checkForNumbersInPassword(String password) {
+            if (password.matches(".*[0-9].*")) {
+                return true;
+        }
+        return false;
+    }
+
+
+
+    public static boolean hasUpperAndLowercase(String password) {
+        boolean hasLowerCase = false;
+        boolean hasUpperCase = false;
+        for (char symbol : password.toCharArray()) {
+            if (Character.isLowerCase(symbol)) {
+                hasLowerCase = true;
+            } else if (Character.isUpperCase(symbol)) {
+                hasUpperCase = true;
+            }
+            // Wenn beide Bedingungen erfüllt sind, beende die Schleife
+            if (hasLowerCase && hasUpperCase) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkForWeakPassword(String password) {
+        String weakPassword1 = "Password1";
+        String weakPassword2 = "Aa345678";
+        if (weakPassword1.equals(password) || weakPassword2.equals(password)) {
+            return true;
+        }
+        return false;
+    }
 }
 
-//checkPasswordlength
 
 
-//checkForNumbersInPassword
 
-//checkForUpperAndLowercase
 
 //checkForUserPasswords
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -17,4 +17,40 @@ class MainTest {
             assertEquals(expected, actual);
         }
 
+    @Test
+    void ifPasswordHasNumbers_thenReturnTrue() {
+        //GIVEN
+        String password = "abc";
+        //WHEN
+        boolean actual = Main.checkForNumbersInPassword(password);
+        //THEN
+        boolean expected = true;
+
+        assertTrue(expected);
+    }
+
+    @Test
+    void ifPasswordHasUpperAndLowercase_thenReturnTrue() {
+        //GIVEN
+        String password = "abc";
+        //WHEN
+        boolean actual = Main.hasUpperAndLowercase(password);
+        //THEN
+        boolean expected = true;
+
+        assertTrue(expected);
+    }
+
+    @Test
+    void ifPasswordIsWeak_thenReturnTrue() {
+        //GIVEN
+        String password = "Aa345678";
+        //WHEN
+        boolean actual = Main.checkForWeakPassword(password);
+        //THEN
+        boolean expected = true;
+
+        assertEquals(expected, actual);
+    }
+
 }
